9863a278338557ca089a4cd4688303448885a7b0
opendingtalk <31611459+opendingtalk@users.noreply.github.com>
2020-05-22 15:32:58 +0800

    Update config.default.js
    update config.default.js

--- config.default.js
+++ config.default.js
@@ -1,5 +1,11 @@
 module.exports = {
     oapiHost: 'https://oapi.dingtalk.com',
+    /**
+     * 开发者后台->应用开发-企业内部应用->选择您创建的小程序->应用首页-查看详情->查看AppKey
+     */
     appkey: '', 
+     /**
+     * 开发者后台->应用开发-企业内部应用->选择您创建的小程序->应用首页-查看详情->查看AppKey
+     */
     appsecret: ''
 }
